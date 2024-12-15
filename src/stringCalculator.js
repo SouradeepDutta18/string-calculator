a9ef1121f63b4f599c3f8686449ec3b1214f68aa
Souradeep Dutta <souradeepdutta2001@gmail.com>
2024-12-16 02:01:41 +0530

    Implement add method to handle empty string

---
+++ src/stringCalculator.js
@@ -0,0 +1,8 @@
+class StringCalculator {
+    static add(input) {
+      return 0;
+    }
+  }
+  
+  module.exports = StringCalculator;
+  
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,3 +1,5 @@
+const StringCalculator = require('../src/stringCalculator');
+
 test('returns zero for a empty string', () => {
     expect(StringCalculator.add('')).toBe(0);
   });
