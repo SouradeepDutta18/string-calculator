665c18d20718105575486322f48305ce6d32e580
Souradeep Dutta <souradeepdutta2001@gmail.com>
2024-12-16 02:14:16 +0530

    add support for multiple comma seperated numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,9 +1,10 @@
 class StringCalculator {
   static add(input) {
     if (input === '') return 0;
-    return parseInt(input, 10);
+    const numbers = input.split(',').map(Number);
+    return numbers.reduce((sum, n) => sum + n, 0);
   }
 }
 
-
 module.exports = StringCalculator;
+
